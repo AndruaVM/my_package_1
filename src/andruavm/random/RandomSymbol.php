2ecaedfeb80b17d2b526aa21bd6a9c50a3eb603d
Parvus <m.andrey.valeryevich@gmail.com>
2019-07-05 10:18:36 +0300

    comsoer

---
+++ src/andruavm/random/RandomSymbol.php
@@ -0,0 +1,17 @@
+<?php
+namespace andruavm\random;
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 05.07.2019
+ * Time: 9:58
+ */
+class RandomSymbol
+{
+    public function getSymbol(){
+
+        return chr(mt_rand(0,1000));
+    }
+}
+
+echo (new RandomSymbol())->getSymbol();
